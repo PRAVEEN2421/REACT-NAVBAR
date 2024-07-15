4113653ed37390cd3aca0252d65204a8406b04da
Sai Praveen <162142454+PRAVEEN2421@users.noreply.github.com>
2024-07-15 22:33:03 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+       <h1>Hi, I'm PRAVEEN </h1>
+            <h3>Full Stock Developer</h3>
+            <p>Gud Morning Mam, First of all, thank you for giving me this opportunity to introduce My Self.
+                My name is Praveen Raj. I am from sivagangai. I have completed my diploma in mechanical engineering from 
+                J Groups polytechnic college alangudi. My strength are I am self motivating person and I have strong 
+                communication skill also. my shortn term goal is to get a job in a reputed company and long term hole is to 
+                achieve a good position like yours. Where i can build my career and help the organization too.
+            </p>
+    </div>
+  );
+};
